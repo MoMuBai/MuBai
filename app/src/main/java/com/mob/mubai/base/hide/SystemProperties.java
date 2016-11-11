22a9297631a9d3afe6ba779e3b71667e7f8e1990
mubai <quickly520@126.com>
2016-11-11 14:17:49 +0800

    添加需要反射类和方法'

---
+++ app/src/main/java/com/mob/mubai/base/hide/SystemProperties.java
@@ -0,0 +1,33 @@
+package com.mob.mubai.base.hide;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by mubai on 2016/11/11.
+ * 反射出系统的方法
+ */
+
+public class SystemProperties {
+    public static String get(String key){
+        String value = "";
+        Class<?> cl = null;
+        try {
+            cl = Class.forName("android.os.SystemProperties");
+            Method hideMethod = cl.getMethod("get",String.class);
+            Object o = cl.newInstance();
+            value = (String) hideMethod.invoke(o,key);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+}
--- app/src/main/java/com/mob/mubai/ui/activity/ReflectionActivity.java
+++ app/src/main/java/com/mob/mubai/ui/activity/ReflectionActivity.java
@@ -4,7 +4,11 @@ import android.app.ActivityManager;
 
 import com.mob.mubai.R;
 import com.mob.mubai.base.BaseActivity;
+import com.mob.mubai.base.hide.SystemProperties;
+import com.mob.mubai.base.utils.L;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -32,11 +36,36 @@ public class ReflectionActivity extends BaseActivity {
     }
 
     private void getReflection() {
-        String className = "";
+        String className = "com.mob.mubai.base.hide.Worker";
         try {
-            Class c = Class.forName(className);
-            
+
+            Class c_1 = Class.forName(className);
+            Object o_1 = c_1.newInstance();
+            L.d(TAG,"反射出无参的构造方法并得到对象:"+o_1.toString());
+
+            Class c_2 = Class.forName(className);
+            Constructor constructor_1 = c_2.getConstructor(int.class,String.class);
+            Object o_2 = constructor_1.newInstance(23,"MuBai");
+            L.d(TAG,"反射出带参数的构造方法并得到对象"+o_2.toString());
+
+
+            Class c_3 = Class.forName(className);
+            Field field_1 = c_3.getDeclaredField("age");
+            Field field_2 = c_3.getField("id");
+            L.d(TAG,"获取类的私有字段"+field_1);
+            L.d(TAG,"获取类的公有字段"+field_2);
+
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
     }
