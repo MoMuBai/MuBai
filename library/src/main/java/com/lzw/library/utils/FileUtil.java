01afc41b532267748598f18ba377acb17a89ac11
quickly520@126.com <quickly520@126.com>
2017-02-23 10:44:43 +0800

    添加复制文件

--- library/src/main/java/com/lzw/library/utils/FileUtil.java
+++ library/src/main/java/com/lzw/library/utils/FileUtil.java
@@ -3,10 +3,16 @@ package com.lzw.library.utils;
 import android.content.Context;
 import android.os.Environment;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
 /**
  * 文件操作工具类
@@ -152,6 +158,70 @@ public class FileUtil {
                         return delDir(source, true);
                 } else {
                         return delFile(source);
+                }
+        }
+
+        /**
+         * 使用文件通道的方式复制文件
+         *
+         * @param s 源文件
+         * @param t 目标文件
+         */
+        public static void fileChannelCopy(File s, File t) {
+                FileInputStream fi = null;
+                FileOutputStream fo = null;
+                FileChannel in = null;
+                FileChannel out = null;
+                try {
+                        fi = new FileInputStream(s);
+                        fo = new FileOutputStream(t);
+                        in = fi.getChannel();//得到对应的文件通道
+                        out = fo.getChannel();//得到对应的文件通道
+                        in.transferTo(0, in.size(), out);//连接两个通道，并且从in通道读取，然后写入out通道
+                } catch (IOException e) {
+                        e.printStackTrace();
+                } finally {
+                        try {
+                                fi.close();
+                                in.close();
+                                fo.close();
+                                out.close();
+                        } catch (IOException e) {
+                                e.printStackTrace();
+                        }
+                }
+        }
+
+        /**
+         * 普通的缓冲输入输出流复制文件
+         *
+         * @param s 源文件
+         * @param t 目标文件
+         */
+        public static void copy(File s, File t) {
+                InputStream inputStream = null;
+                OutputStream outputStream = null;
+                try {
+                        inputStream = new BufferedInputStream(new FileInputStream(s));
+                        outputStream = new BufferedOutputStream(new FileOutputStream(t));
+                        byte[] bytes = new byte[2048];
+                        int i;
+                        try {
+                                while ((i = inputStream.read(bytes)) != -1) {
+                                        outputStream.write(bytes[i]);
+                                }
+                        } catch (IOException e) {
+                                e.printStackTrace();
+                        }
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } finally {
+                        try {
+                                inputStream.close();
+                                outputStream.close();
+                        } catch (IOException e) {
+                                e.printStackTrace();
+                        }
                 }
         }
 
