572da5f51aae2afab721c1b0d803f39a90165a94
quickly520@126.com <quickly520@126.com>
2017-03-08 15:00:52 +0800

    添加验证证书的方法

--- app/src/main/java/com/mb/mubai/base/network/OkHttpManager.java
+++ app/src/main/java/com/mb/mubai/base/network/OkHttpManager.java
@@ -10,6 +10,7 @@ import com.mb.mubai.R;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -17,13 +18,16 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.security.interfaces.RSAPublicKey;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Cache;
 import okhttp3.CacheControl;
@@ -87,13 +91,134 @@ public class OkHttpManager {
                 return okHttpClient;
         }
 
+
         /**
-         * 绑定证书
+         * 错误的证书验证方法
+         * 忽略了检验服务端证书
+         *
+         * @return Error
+         * @throws Exception
+         */
+        public static SSLSocketFactory getErrorSSLSocketFactory() throws Exception {
+                //创建一个不验证证书链的证书信任管理器。
+                final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(
+                                  java.security.cert.X509Certificate[] chain,
+                                  String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(
+                                  java.security.cert.X509Certificate[] chain,
+                                  String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return new java.security.cert.X509Certificate[0];
+                        }
+                }};
+
+                // Install the all-trusting trust manager
+                final SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustAllCerts,
+                          new java.security.SecureRandom());
+                // Create an ssl socket factory with our all-trusting manager
+                return sslContext
+                          .getSocketFactory();
+        }
+
+
+        /**
+         * 客户端没有内置证书的情况
+         *
+         * @return
+         * @throws Exception
+         */
+        public static SSLSocketFactory getSSLSocketFactory() throws Exception {
+                // Create a trust manager that does not validate certificate chains
+                final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                        //证书中的公钥
+                        public static final String PUB_KEY = "3082010a0282010100d52ff5dd432b3a05113ec1a7065fa5a80308810e4e181cf14f7598c8d553cccb7d5111fdcdb55f6ee84fc92cd594adc1245a9c4cd41cbe407a919c5b4d4a37a012f8834df8cfe947c490464602fc05c18960374198336ba1c2e56d2e984bdfb8683610520e417a1a9a5053a10457355cf45878612f04bb134e3d670cf96c6e598fd0c693308fe3d084a0a91692bbd9722f05852f507d910b782db4ab13a92a7df814ee4304dccdad1b766bb671b6f8de578b7f27e76a2000d8d9e6b429d4fef8ffaa4e8037e167a2ce48752f1435f08923ed7e2dafef52ff30fef9ab66fdb556a82b257443ba30a93fda7a0af20418aa0b45403a2f829ea6e4b8ddbb9987f1bf0203010001";
+
+                        @Override
+                        public void checkClientTrusted(
+                                  java.security.cert.X509Certificate[] chain,
+                                  String authType) throws CertificateException {
+
+                        }
+
+                        //客户端并为对ssl证书的有效性进行校验
+                        @Override
+                        public void checkServerTrusted(
+                                  java.security.cert.X509Certificate[] chain,
+                                  String authType) throws CertificateException {
+                                if (chain == null) {
+                                        throw new IllegalArgumentException("checkServerTrusted:x509Certificate array isnull");
+                                }
+
+                                if (!(chain.length > 0)) {
+                                        throw new IllegalArgumentException("checkServerTrusted: X509Certificate is empty");
+                                }
+
+                                if (!(null != authType && authType.equalsIgnoreCase("RSA"))) {
+                                        throw new CertificateException("checkServerTrusted: AuthType is not RSA");
+                                }
+
+                                // Perform customary SSL/TLS checks
+                                try {
+                                        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+                                        tmf.init((KeyStore) null);
+                                        for (TrustManager trustManager : tmf.getTrustManagers()) {
+                                                ((X509TrustManager) trustManager).checkServerTrusted(chain, authType);
+                                        }
+                                } catch (Exception e) {
+                                        throw new CertificateException(e);
+                                }
+                                // Hack ahead: BigInteger and toString(). We know a DER encoded Public Key begins
+                                // with 0×30 (ASN.1 SEQUENCE and CONSTRUCTED), so there is no leading 0×00 to drop.
+                                RSAPublicKey pubkey = (RSAPublicKey) chain[0].getPublicKey();
+
+                                String encoded = new BigInteger(1 /* positive */, pubkey.getEncoded()).toString(16);
+                                // Pin it!
+                                final boolean expected = PUB_KEY.equalsIgnoreCase(encoded);
+
+                                if (!expected) {
+                                        throw new CertificateException("checkServerTrusted: Expected public key: "
+                                                  + PUB_KEY + ", got public key:" + encoded);
+                                }
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return new java.security.cert.X509Certificate[0];
+                        }
+                }};
+
+                // Install the all-trusting trust manager
+                final SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustAllCerts,
+                          new java.security.SecureRandom());
+                // Create an ssl socket factory with our all-trusting manager
+                return sslContext
+                          .getSocketFactory();
+        }
+
+        /**
+         * 客户端内置证书的情况
+         * 需要实现本地证书的加载
+         *
+         * @param context
+         * @param certificates
+         * @return
          */
         protected static SSLSocketFactory getSSLSocketFactory(Context context, int[] certificates) {
                 if (context == null) {
                         throw new NullPointerException("context == null");
                 }
+
+                //CertificateFactory用来证书生成
                 CertificateFactory certificateFactory;
                 try {
                         certificateFactory = CertificateFactory.getInstance("X.509");
@@ -101,16 +226,22 @@ public class OkHttpManager {
                         keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
                         keyStore.load(null, null);
                         for (int i = 0; i < certificates.length; i++) {
+                                //读取本地证书
                                 InputStream certificate = context.getResources().openRawResource(certificates[i]);
                                 keyStore.setCertificateEntry(String.valueOf(i), certificateFactory.generateCertificate(certificate));
                                 if (certificate != null) {
                                         certificate.close();
                                 }
                         }
+
+                        //Create an SSLContext that uses our TrustManager
                         SSLContext sslContext = null;
                         sslContext = SSLContext.getInstance("TLS");
+
+                        //Create a TrustManager that trusts the CAs in our keyStore
                         TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
                         trustManagerFactory.init(keyStore);
+
                         sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
                         return sslContext.getSocketFactory();
                 } catch (CertificateException e) {
