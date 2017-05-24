39ffb25886b29010412d5fdd7456cb44745b4521
quickly520@126.com <quickly520@126.com>
2017-05-24 17:21:13 +0800

    添加跨进程通信的项目

---
+++ app/src/main/java/com/mb/mubai/view/widget/SafeWebView.java
@@ -0,0 +1,28 @@
+package com.mb.mubai.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * @author: lzw
+ * //
+ * @date: 2017/4/17 下午3:03
+ * //
+ * @desc:
+ */
+
+public class SafeWebView extends WebView {
+    public SafeWebView(Context context) {
+        super(context);
+    }
+
+    public SafeWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SafeWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
---
+++ progress/src/main/java/com/mubai/progress/LocalCastielService.java
@@ -0,0 +1,22 @@
+package com.mubai.progress;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * @author: lzw
+ * //
+ * @date: 2017/5/23 下午3:14
+ * //
+ * @desc:
+ */
+
+public class LocalCastielService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
