cd0e98d2b7e5ab1850102e8b7594781e48b96236
quickly520@126.com <quickly520@126.com>
2017-12-05 16:54:28 +0800

    feature 添加圆角ImageView

---
+++ app/src/main/java/com/mb/mubai/data/bean/TestData.java
@@ -0,0 +1,12 @@
+package com.mb.mubai.data.bean;
+
+import java.util.List;
+
+/**
+ * @author: lzw
+ * @date: 2017/12/5 下午1:57
+ * @desc:
+ */
+
+public class TestData {
+}
--- app/src/main/java/com/mb/mubai/ui/test/activity/SeekBarActivity.java
+++ app/src/main/java/com/mb/mubai/ui/test/activity/SeekBarActivity.java
@@ -1,20 +1,29 @@
 package com.mb.mubai.ui.test.activity;
 
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 import com.lzw.library.utils.To;
 import com.mb.mubai.R;
 import com.mb.mubai.base.BaseActivity;
 import com.mb.mubai.ui.test.contract.SeekBarContract;
 import com.mb.mubai.ui.test.model.SeekBarModel;
 import com.mb.mubai.ui.test.presenter.SeekBarPresenter;
+import com.mb.mubai.view.widget.ShapedImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -58,6 +67,8 @@ public class SeekBarActivity extends BaseActivity<SeekBarPresenter, SeekBarModel
     SeekBar seekBarOther;
     @Bind(R.id.activity_second)
     LinearLayout activitySecond;
+    @Bind(R.id.image)
+    ShapedImageView image;
 
     @Override
     protected int getLayout() {
@@ -73,6 +84,24 @@ public class SeekBarActivity extends BaseActivity<SeekBarPresenter, SeekBarModel
     protected void initData() {
         mPresenter.myPost();
 //                mPresenter.start();
+        Glide.with(this).load("http://o7ej9iew1.bkt.clouddn.com/171129033759019.jpg").into(image);
+        Gson gson = new Gson();
+
+
+        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObject1 = new JSONObject();
+        JSONObject jsonObject2 = new JSONObject();
+
+        JSONArray jsonArray = new JSONArray();
+        try {
+            jsonArray.put("123456");
+            jsonObject1.put("0", jsonArray);
+            jsonObject.put("taskDevices", jsonObject1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String json = gson.toJson(jsonObject);
+        Log.d("SeekBarActivity", json);
     }
 
     @OnClick(R.id.back)
