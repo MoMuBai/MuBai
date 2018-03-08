97ee111cf50c3da5a122129aa4b7d9d90d2f9ec4
林志文 <quickly520@126.com>
2018-03-08 11:35:49 +0800

    feature 添加不同的单例实现方式

---
+++ app/src/main/java/com/mb/mubai/base/util/SingleEnumHelper.java
@@ -0,0 +1,15 @@
+package com.mb.mubai.base.util;
+
+/**
+ * Author: lzw
+ * Date: 2018/3/8
+ * Description: This is SingleEnumHelper
+ */
+
+public enum SingleEnumHelper {
+    INSTANCE;
+
+    public void otherMethod() {
+
+    }
+}
--- app/src/main/java/com/mb/mubai/base/util/SingletonHelper.java
+++ app/src/main/java/com/mb/mubai/base/util/SingletonHelper.java
@@ -11,7 +11,59 @@ public class SingletonHelper {
     private static SingletonHelper ourInstance = null;
 
     /**
-     * 加锁
+     * 饿汉
+     */
+    private static SingletonHelper inInstance = new SingletonHelper();
+
+    private volatile static SingletonHelper singletonHelper;
+
+
+    private SingletonHelper() {
+    }
+
+
+    /**
+     * 双重加锁
+     *
+     * @return
+     */
+    public static SingletonHelper getSingletonHelper() {
+        if (null == singletonHelper) {
+            synchronized (SingletonHelper.class) {
+                if (null == singletonHelper) {
+                    singletonHelper = new SingletonHelper();
+                }
+            }
+        }
+        return singletonHelper;
+    }
+
+    /**
+     * 饿汉，变种
+     */
+    static {
+        inInstance = new SingletonHelper();
+    }
+
+
+    /**
+     * 静态内部类
+     */
+    private static class SingletonHolder {
+        private static final SingletonHelper SINGLETON_HELPER = new SingletonHelper();
+    }
+
+
+    public static final SingletonHelper getOurInstance() {
+        return SingletonHolder.SINGLETON_HELPER;
+    }
+
+    public static SingletonHelper getInInstance() {
+        return inInstance;
+    }
+
+    /**
+     * 懒汉，线程安全
      *
      * @return
      */
@@ -26,6 +78,17 @@ public class SingletonHelper {
         return ourInstance;
     }
 
-    private SingletonHelper() {
-    }
+    //    /**
+//     * 懒汉，线程不安全
+//     *
+//     * @return
+//     */
+//    public static SingletonHelper getInstance() {
+//        if (ourInstance == null) {
+//            ourInstance = new SingletonHelper();
+//        }
+//        return ourInstance;
+//    }
+//
+//
 }
