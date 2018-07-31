6b32bd86379e74f93797ef41e2c59b0dea1720c5
林志文 <quickly520@126.com>
2018-07-31 14:37:27 +0800

    添加一个管理Application的静态数组

---
+++ model_component/src/main/java/com/zje/iot/model_component/ComponentConfig.java
@@ -0,0 +1,16 @@
+package com.zje.iot.model_component;
+
+/**
+ * Author: lzw
+ * Date: 2018/7/31
+ * Description: This is ComponentConfig
+ */
+
+public class ComponentConfig {
+    
+    private static final String userApp = "com.lzw.mode_user.App";
+
+    public static String[] modelApps = {
+            userApp
+    };
+}
